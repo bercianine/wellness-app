116f01742bbf4610d9dbc7bdd727bc1d7411bc15
Sofia Bercianine <sfbercianine@gmail.com>
2023-11-20 16:08:33 +0100

    generated the booking model and linked the relations

---
+++ db/migrate/20231120150724_create_bookings.rb
@@ -0,0 +1,12 @@
+class CreateBookings < ActiveRecord::Migration[7.1]
+  def change
+    create_table :bookings do |t|
+      t.date :date
+      t.datetime :start_time
+      t.references :user, null: false, foreign_key: true
+      t.references :activity, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_20_142805) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_20_150724) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,17 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_20_142805) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_activities_on_user_id"
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "date"
+    t.datetime "start_time"
+    t.bigint "user_id", null: false
+    t.bigint "activity_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["activity_id"], name: "index_bookings_on_activity_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,4 +52,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_20_142805) do
   end
 
   add_foreign_key "activities", "users"
+  add_foreign_key "bookings", "activities"
+  add_foreign_key "bookings", "users"
 end
