02eda491c55a3d162bd7b2365b3f4c450d178495
Sofia Bercianine <sfbercianine@gmail.com>
2023-11-20 16:49:47 +0100

    cleaned up the comments and extra lines in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "pages#home"
-
-  Rails.application.routes.draw do
-    resources :activities, only: [:index, :show, :new, :create] do
-      resources :bookings, only: [:new, :create]
-    end
+  resources :activities, only: [:index, :show, :new, :create] do
+    resources :bookings, only: [:new, :create]
   end
-
-
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  # get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
