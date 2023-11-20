fb854dee037228a93cd3d0963bb25a72a80fa60c
Elena Prousali <helenap_sal@yahoo.com>
2023-11-20 16:40:48 +0100

    adding activities and bookings routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
 
   root to: "pages#home"
 
+  Rails.application.routes.draw do
+    resources :activities, only: [:index, :show, :new, :create] do
+      resources :bookings, only: [:new, :create]
+    end
+  end
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
