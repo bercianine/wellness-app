c5c9000a2435754fafc453ba2991a1efd1e3a705
Tobias Groenland <cyber@tobiasgroenland.nl>
2023-11-21 14:55:25 +0100

    Seedfile created

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,41 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Cleaning time...
+puts 'Cleaning database...'
+Activity.destroy_all
+puts 'Cleaned Activities...'
+User.destroy_all
+puts 'Cleaned Users...'
+Booking.destroy_all
+puts 'Cleaned Bookings...'
+
+puts 'Seeding goodness and coder happiness...'
+puts 'Creating | Activities | Bookings | Users ...'
+
+# HOST USER 1A
+user1a = User.create(email: 'host-a1@host.com', password: 'password', username: 'host 1a')
+
+# HOST USER 1B
+user1b = User.create(email: 'host-1b@host.com', password: 'password', username: 'host 1b')
+
+# GUEST USER 2A
+user2a = User.create(email: 'guest-2a@guest.com', password: 'password', username: 'guest 2a')
+
+# GUEST USER 2B
+user2b = User.create(email: 'guest-2b@guest.com', password: 'password', username: 'guest 2b')
+
+# Activities
+Activity.create(user_id: user1a.id, name: 'Stone Massage', price: 500, location: 'Dam,Amsterdam', duration: 60, image_url: 'https://source.unsplash.com/random/?stone massage/1280*1280')
+Activity.create(user_id: user1b.id, name: 'Group Yoga', price: 725, location: 'Grote Markt, The Hague', duration: 90, image_url: 'https://source.unsplash.com/random/?group yoga/1280*1280')
+Activity.create(user_id: user1a.id, name: 'Group Spa', price: 150, location: 'Koolsingel, Rotterdam', duration: 180, image_url: 'https://source.unsplash.com/random/?group spa/1280*1280')
+Activity.create(user_id: user2b.id, name: 'Ayurvedic Massage', price: 135, location: 'NS Central Station, Delft', duration: 60, image_url: 'https://source.unsplash.com/random/?Ayurvedic Massage/1280*1280')
+Activity.create(user_id: user2a.id, name: 'Sauna day', price: 750, location: 'Europaplein, Rotterdam', duration: 500, image_url: 'https://source.unsplash.com/random/?Sauna Day/1280*1280')
+Activity.create(user_id: user2b.id, name: 'Breathing experience', price: 750, location: 'Kanaalkade, Alkmaar', duration: 60, image_url: 'https://source.unsplash.com/random/?Pranayama/1280*1280')
+Activity.create(user_id: user1a.id, name: 'Holistic Harmoney', price: 275, location: 'NS Central Station, Delft', duration: 75, image_url: 'https://source.unsplash.com/random/?Holistic Harmoney/1280*1280')
+Activity.create(user_id: user1b.id, name: 'Acupuncture', price: 125, location: 'Grote Markt, Groningen', duration: 60, image_url: 'https://source.unsplash.com/random/?Acupuncture/1280*1280')
+
+# Finished
+post.save
+puts 'Finished: All good, Happy coding ;-)'
