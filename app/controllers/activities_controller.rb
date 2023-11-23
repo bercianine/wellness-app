be24255c31b831a3284cab0e8f35dcef1a978529
Elena Prousali <helenap_sal@yahoo.com>
2023-11-23 16:39:06 +0100

    implemented and customized map

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -3,10 +3,22 @@ class ActivitiesController < ApplicationController
 
   def index
     @activities = Activity.all
+    @markers = @activities.geocoded.map do |activity|
+      {
+        lat: activity.latitude,
+        lng: activity.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: {activity: activity})
+      }
+    end
   end
 
   def show
     @activity = Activity.find(params[:id])
+    @markers = [{
+        lat: @activity.latitude,
+        lng: @activity.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: {activity: @activity})
+      }]
   end
 
   def new
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,4 +4,7 @@ class Activity < ApplicationRecord
   has_one_attached :photo
 
   validates :name, :price, :location, :duration, presence: true
+
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
